f29cb7941296f34abcbe6739fb75d9d2b0c931d4
Amal Akhmadinurov <amal.akhmadinurov@gmail.com>
2024-06-07 13:49:30 +0200

    Chat selection added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 import "./scss/App.scss";
-import ModalWindow from './components/modal-window';
+import ModalWindow from './components/ModalWindow';
+import Chat from './abstractions/Chat';
 
 
 function App() {
   
-  const [visible, setVisible] = useState(false);
+  const [currentChat, setCurrentChat] = useState(0);
+  const [chats, setChats] = useState({});
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(()=>
+  {
+    if(Object.keys(chats).length == 0)
+    {
+      setChats({"1":new Chat("Test user", "", 1,1,[]), 2:new Chat("Test user 2", "", 2,2,[])})
+    }
+  });
+  let chatsList = new Array(chats.length);
+  Object.keys(chats).forEach((chatId,i)=>
+  {
+  
+    let chat = chats[chatId];
+    chatsList[i] = <div className={`chat-item ${currentChat.id == chat.id ? 'selected':''}`} onClick={()=>{selectedChat(chatId)}} key={chatId}>
+          <img src={`${chat.avatar}`} className='avatar-img'/>
+          <p>{chat.userName}</p>
+    </div>
+  })
+  function selectedChat(chatId)
+  {
+      setCurrentChat(chats[chatId]);
+  }
+  
 
   return (
     <>
@@ -18,28 +45,20 @@ function App() {
               <img className='avatar-img'/>
               <div className='add-info'>
                 <h3>John Johnson</h3>
-                <button className='link'>Change</button>
+                <button className='link' onClick={()=>{setIsVisible(true)}}>Change</button>
 
               </div>
             </div>
-            <div className='chat-item'>
-              <img src="" className='avatar-img'/>
-              <p>Amal</p>
-            </div>
-            <div className='chat-item'>
-              <img src="" className='avatar-img'/>
-              <p>Amal</p>
-            </div>
-            <div className='chat-item'>
-              <img src="" className='avatar-img'/>
-              <p>Amal</p>
-            </div>
+            {chatsList}
+            
+            
+            
         </div>
         <div className='chat'>
           <div className='top-bar'>
             <img src="" className='avatar-img'/>
             <div>
-              <p>Tester</p>
+              <p>{currentChat.userName}</p>
             </div>
           </div>
           <div className="messages-container">
@@ -48,12 +67,12 @@ function App() {
           <div class="bottom-bar">
             <button className='clear attach-btn'><FontAwesomeIcon icon={faPlus} /> </button>
             <input type="text" placeholder='Message...' className='chat-text-input'/>
-            <button onClick={()=>{setVisible(true)}} className='clear send-btn'><FontAwesomeIcon icon={faPaperPlane} /> </button>
-            
+            <button className='clear send-btn'><FontAwesomeIcon icon={faPaperPlane} /> </button>
+          
           </div>
 
         </div>
-        <ModalWindow title="Test" isVisible={visible} onClose={()=>setVisible(false)}></ModalWindow>
+        <ModalWindow title="Test" isVisible={isVisible} onClose={()=>setIsVisible(false)} ></ModalWindow>
       </main>
     </>
   )
---
+++ src/abstractions/Chat.js
@@ -0,0 +1,12 @@
+export default class Chat
+{
+    userName;avatar;userId;id;messages;
+    constructor(userName, avatar, id, userId, messages=[])
+    {
+        this.userName = userName;
+        this.avatar = avatar;
+        this.id = id;
+        this.userId = userId;
+        this.messages = messages;
+    }
+}
