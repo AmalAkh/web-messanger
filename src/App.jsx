92db0b22e8d2c656f1f4c24ceb1bfb54f0046dd5
Amal Akhmadinurov <amal.akhmadinurov@gmail.com>
2024-06-07 12:14:23 +0200

    Modal window

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 import "./scss/App.scss";
+import ModalWindow from './components/modal-window';
 
 
 function App() {
   
+  const [visible, setVisible] = useState(false);
 
   return (
     <>
@@ -46,11 +48,12 @@ function App() {
           <div class="bottom-bar">
             <button className='clear attach-btn'><FontAwesomeIcon icon={faPlus} /> </button>
             <input type="text" placeholder='Message...' className='chat-text-input'/>
-            <button className='clear send-btn'><FontAwesomeIcon icon={faPaperPlane} /> </button>
+            <button onClick={()=>{setVisible(true)}} className='clear send-btn'><FontAwesomeIcon icon={faPaperPlane} /> </button>
             
           </div>
 
         </div>
+        <ModalWindow title="Test" isVisible={visible} onClose={()=>setVisible(false)}></ModalWindow>
       </main>
     </>
   )
---
+++ src/components/modal-window.jsx
@@ -0,0 +1,55 @@
+import { useRef } from "react";
+import "./../scss/ModalWindow.scss";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function ModalWindow({title,children, isVisible=false, onClose=()=>{} })
+{
+    const modalBack = useRef(null);
+    const wasVisible = useRef(null);
+    
+    if(modalBack.current != null)
+    {
+        if(isVisible)
+        {
+            modalBack.current.classList.remove("hidden");
+            modalBack.current.classList.add("show-anim");
+        
+        }else
+        {
+            
+            modalBack.current.classList.add("hide-anim");
+
+        }
+    }
+    wasVisible.current = isVisible;
+    function finishAnimation()
+    {
+        
+        if(!isVisible)
+        {
+            
+            modalBack.current.classList.remove("hide-anim");
+            modalBack.current.classList.add("hidden");
+            
+        }else
+        {
+            modalBack.current.classList.remove("show-anim");
+
+        }
+    }
+    return <>
+        <div ref={modalBack} className={`modal-window-background hidden`} onAnimationEnd={finishAnimation}>
+            <div className="modal-window">
+                <div className="modal-window-header">
+                    <h4>{title}</h4>
+                    <button onClick={onClose} className="close"><FontAwesomeIcon icon={faXmark} /></button>
+                </div>
+                <div className="modal-window-body">
+                    {children}
+                </div>
+
+            </div>
+        </div>
+    </>
+}
