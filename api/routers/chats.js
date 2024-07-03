53e5b30aa524f727a0dae145201dd07d728e0344
Amal Akhmadinurov <amal.akhmadinurov@gmail.com>
2024-07-03 09:13:15 +0200

    Retrieving messages on backend

--- api/routers/chats.js
+++ api/routers/chats.js
@@ -71,5 +71,19 @@ router.delete("/:id",authorizationMiddleware, async (req, res)=>
     
 })
 
+router.get("/:id/messages/:offset", authorizationMiddleware, async(req,res)=>
+{
+    const pool = setupDBConnection();
+    
+    const [rows, _] = await pool.query("SELECT text, date, id, (userid = ?) as isLocal FROM messages WHERE chatid = ? ORDER BY DATE DESC LIMIT 10 OFFSET ?", [res.locals.userId, req.params.id, Number(req.params.offset)]);
+
+    
+    res.send(rows.map((message)=>
+    {
+        return {...message, isLocal:message.isLocal == 1};
+    }));
+    
+});
+
 
 module.exports = router;
