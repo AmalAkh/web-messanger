52614eab2f4c448c769a9ce7466a72ccff777dc4
Amal Akhmadinurov <amal.akhmadinurov@gmail.com>
2024-08-06 15:04:12 +0200

    AutoSizeTextArea component probelms fixed

--- src/components/AutoSizeTextArea.jsx
+++ src/components/AutoSizeTextArea.jsx
@@ -3,21 +3,31 @@ import { useEffect, useState, useRef } from "react";
 export default function AutoSizeTextArea({placeholder="",value="", onInput = (e)=>{}})
 {
     const textarea = useRef(null);
+    
+
     useEffect(()=>
     {
 
-        setMessageInputSize(value.split("\n").length <=4 ? value.split("\n").length:4);
+        textarea.current.style.height = "auto";
+
+        
+        textarea.current.style.height = `${textarea.current.scrollHeight}px`;
         
     },[value]);
     function valueChanged(e)
     {
         
-        setMessageInputSize(e.target.value.split("\n").length <=4 ? e.target.value.split("\n").length:4);
+        textarea.current.style.height = "auto";
+
+        
+        textarea.current.style.height = `${textarea.current.scrollHeight}px`;
+        
+        
         onInput(e);
     }
     const [messageInputSize, setMessageInputSize] = useState(1);
     return <>
-    <textarea placeholder={placeholder} value={value}  ref={textarea}  onInput={(e)=>valueChanged(e)} className={`auto-size-text-area size-${messageInputSize}`}></textarea>
+    <textarea placeholder={placeholder} value={value} rows={1}  ref={textarea}  onInput={(e)=>valueChanged(e)} className={`auto-size-text-area size-${messageInputSize}`}></textarea>
     </>
     
 }
